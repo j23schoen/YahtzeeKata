b1fb2153ddfac096f6b79e8467982efa0d6b7456
j23schoen <j23schoen@gmail.com>
2016-03-12 21:46:52 -0500

    green lighted ones

--- src/Yahtzee.java
+++ src/Yahtzee.java
@@ -4,7 +4,21 @@ public class Yahtzee {
     public int ones(int d1, int d2, int d3, int d4, int d5){
         int total = 0;
 
-
+        if(d1 == 1){
+            total++;
+        }
+        if(d2 == 1){
+            total++;
+        }
+        if(d3 == 1){
+            total++;
+        }
+        if(d4 == 1){
+            total++;
+        }
+        if(d5 == 1){
+            total++;
+        }
 
         return total;
     }
